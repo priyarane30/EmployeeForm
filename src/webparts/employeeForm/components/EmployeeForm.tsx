151d2009e1b2491bc05b0d89c6273733ffa3971b
Adit Pandey <pandeyad@SYNOVERGETECH.COM>
2020-01-09 11:33:19 +0530

    MERGE CHANGES

--- src/webparts/employeeForm/components/EmployeeForm.tsx
+++ src/webparts/employeeForm/components/EmployeeForm.tsx
@@ -7,29 +7,8 @@ import EducationDetail from '../components/tabs/EducationDetail';
 import ProfessionalDetail from '../components/tabs/ProfessionalDetail';
 import HRDetail from '../components/tabs/HRDetail';
 import PayrollDetail from '../components/tabs/PayrollDetail';
-<<<<<<< HEAD
-import { Provider, connect } from 'react-redux';
-import { store } from "../store/ConfigureStore";
-import { SetTabName, GetInitialControlValuesAction, AddNewEmployee } from "./../actions/NewFormControlsValuesAction";
-import { ICommonState } from './../state/ICommonState';
-interface INewFormConnectedDispatch {
-  setTabName: (tabName: ICommonState) => void;
-
-  // Gets the options for dropdown fields
-  getDefaultControlsData: () => void;
-
-  //save data
-  // addNewEmployee: (empData: INewFormState) => void;
-}
-class EmployeeForm extends React.Component<IEmployeeFormProps, {}> {
-
-  componentDidMount() {
-    //this.props.getDefaultControlsData();
-  }
-=======
 import { Provider } from 'react-redux';
 import { store } from "../store/ConfigureStore";
->>>>>>> 3b1c42d2288b731e2ea4a812c26ba6ab2fb7d1e2
 
 export default class EmployeeForm extends React.Component<IEmployeeFormProps, {}> {
   public render(): React.ReactElement<IEmployeeFormProps> {
@@ -59,31 +38,4 @@ export default class EmployeeForm extends React.Component<IEmployeeFormProps, {}
       </Provider>
     );
   }
-<<<<<<< HEAD
-
-
 }
-const mapStateToProps = function (state) {
-  console.log(state)
-  return state;
-}
-
-// Maps dispatch to props
-const mapDispatchToProps = (dispatch): INewFormConnectedDispatch => {
-  return {
-    setTabName: (tabData: ICommonState) => {
-      return dispatch(SetTabName(tabData))
-    },
-    getDefaultControlsData: () => {
-      return dispatch(GetInitialControlValuesAction());
-    },
-    // addNewEmployee: (empData: INewFormState) => {
-    //     return dispatch(AddNewEmployee(empData));
-    // }
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(EmployeeForm);
-=======
-}
->>>>>>> 3b1c42d2288b731e2ea4a812c26ba6ab2fb7d1e2
