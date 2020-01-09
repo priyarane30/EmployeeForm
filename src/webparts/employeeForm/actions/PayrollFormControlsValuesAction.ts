d069c612d2fd617f371ac11d89e2652c6551be0e
Hirvita Rajyaguru <rajyaghi@SYNOVERGETECH.COM>
2020-01-09 11:48:38 +0530

    added payroll

---
+++ src/webparts/employeeForm/actions/PayrollFormControlsValuesAction.ts
@@ -0,0 +1,5 @@
+import { IPayrollState } from '../state/IPayrollState';
+import { ICommonState } from '../state/ICommonState';
+import NewEmployeeService from '../services/NewEmployeeService';
+import { ActionTypes } from '../AppConstants';
+import NewEmpService from '../services/NewEmployeeService';
