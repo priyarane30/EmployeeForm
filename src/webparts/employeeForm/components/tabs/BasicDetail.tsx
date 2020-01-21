05ffabdc099c9fa8ebbb614eb9ff07d60b2a860e
priyarane30 <34854908+priyarane30@users.noreply.github.com>
2020-01-21 19:19:19 +0530

    apply css

--- src/webparts/employeeForm/components/tabs/BasicDetail.tsx
+++ src/webparts/employeeForm/components/tabs/BasicDetail.tsx
@@ -1,12 +1,7 @@
 import * as React from 'react';
-import { Form, Control, Errors, actions, combineForms } from 'react-redux-form';
-import {
-    Dropdown,
-    IDropdown,
-    DropdownMenuItemType,
-    IDropdownOption
-} from "office-ui-fabric-react/lib/Dropdown";
-import { ActionButton, DefaultButton, IButtonProps } from "office-ui-fabric-react/lib/Button";
+import { Form, Control, Errors } from 'react-redux-form';
+
+import { DefaultButton,PrimaryButton } from "office-ui-fabric-react/lib/Button";
 import { TextField } from "office-ui-fabric-react/lib/TextField";
 import { connect } from "react-redux";
 import { GetEmpBasicData, SetTabName, GetEmpListIdByUserEmail, SetEmpIdInStore } from "../../actions/BasicEmpDetailAction";
@@ -16,15 +11,10 @@ import BasicService from '../../services/BasicFormService'
 import { ActionTypes } from '../../AppConstants';
 import { store } from '../../store/ConfigureStore';
 import { DatePicker } from 'office-ui-fabric-react/lib/DatePicker';
+import styles from "../EmployeeForm.module.scss";
 
-const required: any = (val: string) => val && val.length > 0;
-const isEmail = (val) => val && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
-const notSetOrNumber: any = (val: string) => val && val.length === 0 || (Number(val) >= 1);
-const minValue = min => value => value && value < min ? `Must be at least ${min}` : undefined;
-const email = value => value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ? 'Invalid email address' : undefined;
 interface IBasicFormConnectedDispatch {
     //Get Employee Id using Current User Email
-
     setEmpId: (empId) => void;
     setTabName: (tabName: ICommonState) => void;
     // Gets the options for dropdown fields
@@ -33,9 +23,6 @@ interface IBasicFormConnectedDispatch {
     addBasicDetails: (empData: IBasicDetailState) => void;
 }
 
-/*interface IStateBasic {
-    DateofJoining:Date;
-  }*/
 class BasicDetail extends React.Component<any>{
     constructor(props) {
         super(props);
@@ -100,124 +87,149 @@ class BasicDetail extends React.Component<any>{
 
         return (
             <div>
-                <Form model="Basic" onSubmit={(val) => this.handleSubmit(val)}  >
-                    <div className='col'>
-                        <label>First Name:</label>
+                <div className={styles.employeeForm}>
+                    <div className={styles.container}>
+                        <div className={`ms-Grid-row  ms-fontColor-white ${styles.row}`}>
+                            <Form model="Basic" onSubmit={(val) => this.handleSubmit(val)}  >
 
-                        <Control.text model=".FirstName" id='.FirstName' component={TextField}
-                          validators={{
-                            requiredFirstName: (val) => val && val.length,
-                          
-                        }} />
-                         <Errors
-                            model=".FirstName"
-                            messages={{
-                                requiredFirstName: 'Please provide an email address.',
-                            }}
-                        />
+                                <div className='ms-Grid-col ms-u-sm4 block'>
+                                    <label>First Name *:</label>
+                                </div>
+                                <div className="ms-Grid-col ms-u-sm8 block">
+                                    <Control.text model=".FirstName" id='.FirstName' component={TextField} className={styles.marginb}
+                                        validators={{
+                                            requiredFirstName: (val) => val && val.length,
 
-                    </div>
-                    <div className='col'>
-                        <label>Last Name:</label>
-                        <Control.text model=".LastName" id='.LastName' component={TextField}
-                        validators={{
-                            requiredLastName: (val) => val && val.length,
-                          
-                        }}  />
-                         <Errors
-                            model=".LastName"
-                            messages={{
-                                requiredLastName: 'Please provide an email address.',
-                            }}
-                        />
+                                        }} />
+                                    <Errors
+                                        model=".FirstName"
+                                        messages={{
+                                            requiredFirstName: 'Please provide an email address.',
+                                        }}
+                                    />
+
+                                </div>
+                                <div className='ms-Grid-col ms-u-sm4 block'>
+                                    <label>Last Name *:</label>
+                                </div>
+                                <div className="ms-Grid-col ms-u-sm8 block">
+                                    <Control.text model=".LastName" id='.LastName' component={TextField} className={styles.marginb}
+                                        validators={{
+                                            requiredLastName: (val) => val && val.length,
+
+                                        }} />
+                                    <Errors
+                                        model=".LastName"
+                                        messages={{
+                                            requiredLastName: 'Please provide an email address.',
+                                        }}
+                                    />
 
 
-                    </div>
-                    <div className='col'>
-                        <label>Date Of Joining:</label>
-                        <Control model='.DateofJoining' id='.DateofJoining' component={DatePicker}
-                            mapProps={{
-                                value: (props) => { return props.viewValue },
-                                onSelectDate: (props) => { return props.onChange }
-                            }}
-                        ></Control>
-                    </div>
-                    <div className='col'>
-                        <label>Designation:</label>
-                        <Control.select model=".Designation" id=".Designation"  validators={{
-                            requiredDesignationStatus: (val) =>  val && val!="--Select--"
-                          }} >
-                            <option>--Select--</option>
-                            {/*
+                                </div>
+
+                                <div className='ms-Grid-col ms-u-sm4 block'>
+                                    <label>Date Of Joining *:</label>
+                                </div>
+                                <div className="ms-Grid-col ms-u-sm8 block">
+                                    <Control model='.DateofJoining' id='.DateofJoining' component={DatePicker} className={styles.marginb}
+                                        mapProps={{
+                                            value: (props) => { return props.viewValue },
+                                            onSelectDate: (props) => { return props.onChange }
+                                        }}
+                                    ></Control>
+                                </div>
+                                <div className='ms-Grid-col ms-u-sm4 block'>
+                                    <label>Designation *:</label>
+                                </div>
+                                <div className="ms-Grid-col ms-u-sm8 block">
+                                    <Control.select model=".Designation" id=".Designation" className={styles.dropdowncustom} validators={{
+                                        requiredDesignationStatus: (val) => val && val != "--Select--"
+                                    }} >
+                                        <option>--Select--</option>
+                                        {/*
                                 this.props.Basic.designationOptions.map(desig => { return <option key={desig} value={desig}>{desig}</option> })
                             */}
-                            {desigOpt}
-                        </Control.select>
-                        <Errors
-                            model=".Designation"
-                            messages={{
-                                requiredDesignationStatus: 'Please Select Designation.'
-                            }}
-                        />
-                    </div>
-                    <div className='col'>
-                        <label>Technology:</label>
-                        <Control.select model=".Technology" id=".Technology" validators={{
-                            requiredTechnology: (val) =>  val && val!="--Select--"
-                          }}   >
-                            <option>--Select--</option>
-                            {/* {technologies.map(tech => { return <option key={tech} value={tech}>{tech}</option> })} */}
+                                        {desigOpt}
+                                    </Control.select>
+                                    <Errors
+                                        model=".Designation"
+                                        messages={{
+                                            requiredDesignationStatus: 'Please Select Designation.'
+                                        }}
+                                    />
+                                </div>
+                                <div className='ms-Grid-col ms-u-sm4 block'>
+                                    <label>Technology *:</label>
+                                </div>
+                                <div className="ms-Grid-col ms-u-sm8 block">
+                                    <Control.select model=".Technology" id=".Technology" className={styles.dropdowncustom} validators={{
+                                        requiredTechnology: (val) => val && val != "--Select--"
+                                    }}   >
+                                        <option>--Select--</option>
+                                        {/* {technologies.map(tech => { return <option key={tech} value={tech}>{tech}</option> })} */}
 
-                            {techOpts}
-                        </Control.select>
-                        <Errors
-                            model=".Technology"
-                            messages={{
-                                requiredTechnology: 'Please Select Technology.'
-                            }}
-                        />
-                    </div>
-                    <div className='col'>
-                        <label>Company Email:</label>
-                        <Control.text model=".CompanyEmail" id='.CompanyEmail' component={TextField} 
-                         validators={{
-                            requiredEmail: (val) => val && val.length,
-                            isEmail: (val) => (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val)) // ES6 property shorthand
-                        }}/>
-                        <Errors
-                            model=".PersonalEmail"
-                            messages={{
-                                requiredEmail: 'Please provide an email address.',
-                                isEmail: (val) => `${val} is not a valid email.`,
-                            }}
-                        />
+                                        {techOpts}
+                                    </Control.select>
+                                    <Errors
+                                        model=".Technology"
+                                        messages={{
+                                            requiredTechnology: 'Please Select Technology.'
+                                        }}
+                                    />
+                                </div>
+                                <div className='ms-Grid-col ms-u-sm4 block'>
+                                    <label>Company Email *:</label>
+                                </div>
+                                <div className="ms-Grid-col ms-u-sm8 block">
+                                    <Control.text model=".CompanyEmail" id='.CompanyEmail' className={styles.marginb} component={TextField}
+                                        validators={{
+                                            requiredEmail: (val) => val && val.length,
+                                            isEmail: (val) => (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val)) // ES6 property shorthand
+                                        }} />
+                                    <Errors
+                                        model=".PersonalEmail"
+                                        messages={{
+                                            requiredEmail: 'Please provide an email address.',
+                                            isEmail: (val) => `${val} is not a valid email.`,
+                                        }}
+                                    />
+                                </div>
+                                <div >
+                                    <div >
+                                        <div>
+                                            <DefaultButton id="DefaultSubmit"
+                                                primary={true}
+                                                text={"Submit"}
+                                                type="submit" />
+                                        </div>
+
+                                    </div>
+                                </div>
 
 
+                                {/* <button type="submit">Submit</button> */}
+
+
+                            </Form>
+                        </div>
                     </div>
-                    <button type="submit">Submit</button>
-                </Form>
+
+                </div>
             </div>
         );
 
     }
-    private _onSelectDate = (date: Date | null | undefined): void => {
-        this.setState({ DateofJoining: date });
-    };
-
-    private _onFormatDate = (date: Date): string => {
-        return date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear();
-    };
 }
 
 const mapStateToProps = function (state) {
-    // console.log(state)
     return state;
 }
 
 
 // Maps dispatch to props
 const mapDispatchToProps = (dispatch): IBasicFormConnectedDispatch => {
-    debugger
+
     return {
         setEmpId: (empId) => {
             return dispatch(SetEmpIdInStore(empId));
